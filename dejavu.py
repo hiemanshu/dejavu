6a9eec06f7476562a08db806b869426a4886a9a7
Shashi Gowda <connect2shashi@gmail.com>
2011-06-04 20:26:47 +0530

    Include hiemanshu's discovery code

---
+++ dejavu.py
@@ -0,0 +1,34 @@
+import json
+import subprocess
+import string
+import sys
+
+version = '0.1-alpha'
+servers = {}
+
+def discover(ip, port):
+    """Discover ips with the given port open with for the given ip range
+       Example: discover('192.168.2.0/24', '80')"""
+    ip2 = ip
+    for i in string.split(ip, sep='.'):
+        if '/' in i:
+            ip = string.split(i, sep='/')[0]
+            if int(ip) > 255:
+                print 'Invalid IP range'
+                sys.exit(0)
+        else:
+            if int(i) > 255:
+                print 'Invalid IP range'
+                sys.exit(0)
+    cmd1 = '/usr/bin/nmap -v %s -p %s' %(ip2, port)
+    cmd = string.split(cmd1)
+    output =  subprocess.check_output(cmd)
+    outputS = string.split(output, '\n')
+    ipR = []
+    
+    for line in outputS:
+        if 'Discovered open' in line:
+            ipR.append(line.split()[-1])
+
+    return ipR
+
--- dejavu_client.py
+++ dejavu_client.py
@@ -1,11 +1,14 @@
-from bottle import route, run
+from bottle import route, get, post, run
 import dejavu
 import json
 
-@get '/'
+@get('/')
 def introduce():
     print json.dump({"servers": dejavu.servers, "version": dejavu.version})
 
-@post '/server'
+@post('/server')
 def addserver():
-    
+    print request.body
+    dejavu.servers.append(request.body)
+
+run(host='localhost', port=2012)
